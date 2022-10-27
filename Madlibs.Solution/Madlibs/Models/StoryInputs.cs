bf10a671a0465a82c27be16b9c8166a881016d6a
Lucian Miller <lucian.miller185@gmail.com>
2022-10-27 11:46:14 -0700

    Add StoryInputs class

---
+++ Madlibs.Solution/Madlibs/Models/StoryInputs.cs
@@ -0,0 +1,12 @@
+namespace Madlibs.Models 
+{
+  public class StoryInputs
+  {
+    public string Person1 { get; set; }
+    public string Person2 { get; set; }
+    public string Animal { get; set; }
+    public string Exclamation { get; set; }
+    public string Verb { get; set; }
+    public string Noun { get; set; }
+  }
+}
